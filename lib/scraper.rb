c18fffbdb3b7c8764985d4c2895280637bd152b8
Melissa Ramirez <hdez.melissaa@gmail.com>
2020-01-03 00:16:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,9 +4,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student_info= [ ]
+    student_info=[]
     web = Nokogiri::HTML(open(index_url))
-    #binding.pry
     roster = web.css(".student-card")
     roster.each do |info|
       student_info << {:name=>info.css("h4").text,:location=>info.css("p").text,:profile_url=>info.xpath('a').attr('href').text.strip}
@@ -32,16 +31,3 @@ class Scraper
     student_info
   end
 end
-
-
- 
-
-    
-  end
-
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
-end
-
