c79e0d3098c7d69c09adeaebb632f6620c9b71da
Melissa Ramirez <hdez.melissaa@gmail.com>
2020-01-03 00:17:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,25 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.map do |student|
+      self.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do | k,v | 
+      self.send( "#{k}=", v )
+      # binding.pry
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
-
