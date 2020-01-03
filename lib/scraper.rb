6b2b6c4132491c3c96a56645d060c116ad3d8b57
Melissa Ramirez <hdez.melissaa@gmail.com>
2020-01-03 00:16:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,33 @@ class Scraper
     #binding.pry
     roster = web.css(".student-card")
     roster.each do |info|
-      student_info << {:name=>info.css("h4").text,:location=>info.css
+      student_info << {:name=>info.css("h4").text,:location=>info.css("p").text,:profile_url=>info.xpath('a').attr('href').text.strip}
+    end
+    student_info
+  end
+
+
+  def self.scrape_profile_page(profile_url)
+
+    student_info = {}
+
+    web = Nokogiri::HTML(open(profile_url))
+    web.css("div.social-icon-container a").each do |info|
+        url = info.attribute("href").value
+        student_info[:twitter] = url if url.include?("twitter")
+        student_info[:linkedin] = url if url.include?("linkedin")
+        student_info[:github] = url if url.include?("github")
+        student_info[:blog] = url if info.css("img").attribute("src").text.include?("rss")
+    end
+        student_info[:profile_quote] = web.css("div.profile-quote").text
+        student_info[:bio] = web.css("div.bio-content p").text
+    student_info
+  end
+end
+
+
+ 
+
     
   end
 
